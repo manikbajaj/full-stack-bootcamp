ac9dc038048cb69915ac585089dd1e6c966e1c16
Manik <manik@pixelconception.com>
2024-09-19 22:23:35 +0530

    final code for FSDB329

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 let fruits = ["Apple", "Banana", "Orange", "Mango"];
 console.log(fruits[0]); // Output: "Apple"
-console.log(fruits[2]); // Output: "Orange"
 
-let numbers = [10, 20, 30, 40, 50];
+let numbers = new Array(10, 20, 30, 40, 50);
 console.log(numbers[1]); // Output: 20
 console.log(numbers[4]); // Output: 50
 
-let mixedArray = ["Hello", 25, true, null, { name: "John" }, [1, 2, 3]];
-// Extracting a string
-console.log(mixedArray[0]); // Output: "Hello"
+// Adding new items to an array
+fruits.push("Grapes");
+console.log(fruits);
 
-// Extracting an object property
-console.log(mixedArray[4].name); // Output: "John"
+// Mutability
+// Const keyword can be used with Arrays
+const newFruits = fruits;
+// If I use the spread operator it will create a copy of the array
+// const newFruits = [...fruits];
+console.log(newFruits);
 
-// Extracting a nested array
-console.log(mixedArray[5][1]); // Output: 2
+fruits.pop();
+console.log(fruits);
+console.log(newFruits);
