0bdc33427d210b48097f41f665308158f5be46e2
Manik <manik@pixelconception.com>
2024-09-19 19:24:34 +0530

    final code for FSDB321

---
+++ script.js
@@ -0,0 +1,19 @@
+// Example of defininig all the properties of a car
+let carBrand = "BMW";
+let carModel = "7 Series";
+let carColor = "White";
+let carMake = 2024;
+
+// All these properties belong to the same entity Car
+// Types of cars will probably have all these properties
+// There is a way to group all these properties of a Car into a single group or object called Car
+let car = {
+  brand: "BMW",
+  model: "7 Series",
+  color: "White",
+  make: 2024,
+};
+
+// Now each property of the car can be accessed using the car Object or group
+console.log("Car Model:", car.model);
+console.log("Car Brand:", car["brand"]);
