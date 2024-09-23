2764f7f3d2bde0a501a245faa6817f04582b35ac
Manik <manik@pixelconception.com>
2024-09-23 13:57:21 +0530

    Final code for FSDB364

--- script.js
+++ script.js
@@ -1,52 +1,94 @@
-// Lecture: Commonly Used Date Object Methods in JavaScript
+// JavaScript Lecture: Constructor Functions
 
-// // // // // // // //
-// WHY Math Object can be used without instantiatioon and Date cannot be used
-// // // // // // // //
-/**
- * Since Math only contains utility functions (like Math.pow() or Math.random()), there is no need for storing individual state or data. Therefore, you can access and use the methods directly without creating a new instance.
- *
- * Creating a new Date object using new Date() allows you to store, manipulate, and work with unique time values for different scenarios, unlike Math, which just provides utility functions that don’t rely on individual instances.
- *
- * THERE ARE SOME FUNCTIONS ON DATE THAT CANBE USED DIRECTLY like now()
- */
+/* 
+Constructor functions are used to create multiple instances of similar objects. 
+They help create reusable "blueprints" for object creation and are an important part of object-oriented programming (OOP) in JavaScript.
+*/
 
-// **1. Creating a new Date Object**
-// If no arguments are provided, the Date object will be initialized to the current date and time.
-let currentDate = new Date();
-console.log("Current Date and Time:", currentDate);
+// 1. USing Capital C is a naming convention not mandatory
 
-// **2. Date with Specific Date and Time**
-// You can create a Date object with specific date and time values by passing them as arguments.
-let specificDate = new Date(2024, 6, 18, 15, 30, 0); // Year, Month (0-based), Day, Hour, Minute, Second
-console.log("Specific Date and Time:", specificDate);
+function Car(brand, model, year, color) {
+  // Assign properties using 'this'
+  this.brand = brand;
+  this.model = model;
+  this.year = year;
+  this.color = color;
+}
 
-// **3. Get the current year**
-// Using the `getFullYear()` method to get the 4-digit year of the date.
-console.log("Current Year:", currentDate.getFullYear());
+// Creating instances (objects) of the Car constructor
+let car1 = new Car("BMW", "7 Series", 2024, "White");
+let car2 = new Car("Audi", "A6", 2021, "Black");
 
-// **4. Get the current month**
-// Using the `getMonth()` method to get the month (0-based, so January is 0, February is 1, etc.).
-console.log("Current Month (0-based):", currentDate.getMonth()); // 0-based
+console.log(car1); // Output: Car {brand: 'BMW', model: '7 Series', year: 2024, color: 'White'}
+console.log(car2); // Output: Car {brand: 'Audi', model: 'A6', year: 2021, color: 'Black'}
 
-// **5. Get the current day of the month**
-// The `getDate()` method returns the day of the month (1-31).
-console.log("Current Day of the Month:", currentDate.getDate());
+/* 
+Explanation:
+- The constructor function `Car` creates an object with properties: `brand`, `model`, `year`, and `color`.
+- Each time the function is called with the `new` keyword, a new object is created.
+*/
 
-// **6. Get the current day of the week**
-// The `getDay()` method returns the day of the week (0-based, Sunday is 0, Monday is 1, etc.).
-console.log(
-  "Current Day of the Week (0-Sunday, 6-Saturday):",
-  currentDate.getDay()
-);
+// 2. Property Default Values
 
-// **10. Date.now()**
-// This method returns the number of milliseconds that have passed since January 1, 1970 (Unix Epoch).
-let currentTimestamp = Date.now();
-console.log("Current Timestamp:", currentTimestamp);
+/* 
+Default values can be assigned to constructor properties if arguments are not passed. 
+This ensures that even if the user doesn't provide specific values, the object will still have meaningful values.
+*/
 
-// **11. Converting Date to String**
-// Use `toString()`, `toDateString()`, and `toTimeString()` to convert a date to a readable format.
-console.log("Date to String:", currentDate.toString()); // Full string representation
-console.log("Date only String:", currentDate.toDateString()); // Only date
-console.log("Time only String:", currentDate.toTimeString()); // Only time
+function Bike(brand, model, year, color = "Black") {
+  this.brand = brand;
+  this.model = model;
+  this.year = year;
+  this.color = color; // Default value if no color is provided
+}
+
+// Creating Bike objects with and without providing color
+let bike1 = new Bike("Yamaha", "R15", 2021, "Blue");
+let bike2 = new Bike("Ducati", "Panigale", 2023); // No color provided
+
+console.log(bike1); // Output: Bike {brand: 'Yamaha', model: 'R15', year: 2021, color: 'Blue'}
+console.log(bike2); // Output: Bike {brand: 'Ducati', model: 'Panigale', year: 2023, color: 'Black'}
+
+/* 
+Explanation:
+- In the `Bike` constructor, `color` has a default value of "Black". 
+- If the user doesn't provide a color, the default "Black" is assigned to the property.
+*/
+
+// 3. Constructor Function Methods
+
+/* 
+Methods can be added inside constructor functions. These methods are specific to each object instance.
+*/
+
+function Person(firstName, lastName, age) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.age = age;
+
+  // Adding a method inside the constructor
+  this.getFullName = function () {
+    return `${this.firstName} ${this.lastName}`;
+  };
+}
+
+// Creating Person objects
+let person1 = new Person("John", "Doe", 30);
+let person2 = new Person("Jane", "Smith", 25);
+
+console.log(person1.getFullName()); // Output: John Doe
+console.log(person2.getFullName()); // Output: Jane Smith
+
+/* 
+Explanation:
+- The `getFullName` method is added to each object created using the `Person` constructor.
+- Each object can call its own version of this method.
+- Methods defined inside the constructor are specific to each instance of the object.
+*/
+
+// Summary:
+/* 
+- Constructor functions allow for the creation of multiple instances of similar objects.
+- Default values can be assigned to properties to handle cases when arguments are not passed.
+- Methods can be added inside constructor functions to give objects specific behaviors.
+*/
