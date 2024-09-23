79b0b7d6ded94a989fe32be8ccc7dc6fc7ea9710
Manik <manik@pixelconception.com>
2024-09-23 14:36:35 +0530

    Final code for FSDB381

--- script.js
+++ script.js
@@ -1,94 +1,45 @@
-// JavaScript Lecture: Constructor Functions
+// JavaScript Lecture: Spread Operator with Arrays
 
-/* 
-Constructor functions are used to create multiple instances of similar objects. 
-They help create reusable "blueprints" for object creation and are an important part of object-oriented programming (OOP) in JavaScript.
-*/
+// 1. Introduction to Spread Operator with Arrays
+// The spread operator (...) allows an array to be expanded in places where zero or more elements are expected.
+// It essentially "spreads" out the elements of an array into individual elements.
 
-// 1. USing Capital C is a naming convention not mandatory
+let numbers = [1, 2, 3];
+let moreNumbers = [4, 5, 6];
 
-function Car(brand, model, year, color) {
-  // Assign properties using 'this'
-  this.brand = brand;
-  this.model = model;
-  this.year = year;
-  this.color = color;
-}
+// Using spread operator to combine arrays
+let combined = [...numbers, ...moreNumbers];
+console.log("Combined Array:", combined);
+// Output: [1, 2, 3, 4, 5, 6]
 
-// Creating instances (objects) of the Car constructor
-let car1 = new Car("BMW", "7 Series", 2024, "White");
-let car2 = new Car("Audi", "A6", 2021, "Black");
+// 2. Adding Elements to an Array Using Spread Operator
+// You can use the spread operator to easily add new elements to an array without modifying the original array.
 
-console.log(car1); // Output: Car {brand: 'BMW', model: '7 Series', year: 2024, color: 'White'}
-console.log(car2); // Output: Car {brand: 'Audi', model: 'A6', year: 2021, color: 'Black'}
+let moreFruits = ["Grapes", ...fruits, "Pineapple"];
+console.log("New Fruits Array:", moreFruits);
+// Output: ["Grapes", "Apple", "Banana", "Orange", "Pineapple"]
 
-/* 
-Explanation:
-- The constructor function `Car` creates an object with properties: `brand`, `model`, `year`, and `color`.
-- Each time the function is called with the `new` keyword, a new object is created.
-*/
+// 3. Shallow Copy Warning with Spread Operator
+// The spread operator only creates a shallow copy of an array, meaning that nested objects within the array will still be referenced.
 
-// 2. Property Default Values
+let nestedArray = [
+  [1, 2],
+  [3, 4],
+];
+let copiedArray = [...nestedArray];
 
-/* 
-Default values can be assigned to constructor properties if arguments are not passed. 
-This ensures that even if the user doesn't provide specific values, the object will still have meaningful values.
-*/
+// Modifying the nested array in the copied array affects the original array
+copiedArray[0][0] = 99;
+console.log("Original Nested Array:", nestedArray);
+// Output: [[99, 2], [3, 4]]
 
-function Bike(brand, model, year, color = "Black") {
-  this.brand = brand;
-  this.model = model;
-  this.year = year;
-  this.color = color; // Default value if no color is provided
-}
+// 7. Spread Operator with Arrays Inside Objects
+// Spread operator can also be used inside objects to spread arrays as values.
 
-// Creating Bike objects with and without providing color
-let bike1 = new Bike("Yamaha", "R15", 2021, "Blue");
-let bike2 = new Bike("Ducati", "Panigale", 2023); // No color provided
+let objectWithArray = {
+  name: "Sample Object",
+  items: [...fruits],
+};
 
-console.log(bike1); // Output: Bike {brand: 'Yamaha', model: 'R15', year: 2021, color: 'Blue'}
-console.log(bike2); // Output: Bike {brand: 'Ducati', model: 'Panigale', year: 2023, color: 'Black'}
-
-/* 
-Explanation:
-- In the `Bike` constructor, `color` has a default value of "Black". 
-- If the user doesn't provide a color, the default "Black" is assigned to the property.
-*/
-
-// 3. Constructor Function Methods
-
-/* 
-Methods can be added inside constructor functions. These methods are specific to each object instance.
-*/
-
-function Person(firstName, lastName, age) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.age = age;
-
-  // Adding a method inside the constructor
-  this.getFullName = function () {
-    return `${this.firstName} ${this.lastName}`;
-  };
-}
-
-// Creating Person objects
-let person1 = new Person("John", "Doe", 30);
-let person2 = new Person("Jane", "Smith", 25);
-
-console.log(person1.getFullName()); // Output: John Doe
-console.log(person2.getFullName()); // Output: Jane Smith
-
-/* 
-Explanation:
-- The `getFullName` method is added to each object created using the `Person` constructor.
-- Each object can call its own version of this method.
-- Methods defined inside the constructor are specific to each instance of the object.
-*/
-
-// Summary:
-/* 
-- Constructor functions allow for the creation of multiple instances of similar objects.
-- Default values can be assigned to properties to handle cases when arguments are not passed.
-- Methods can be added inside constructor functions to give objects specific behaviors.
-*/
+console.log("Object with Array Inside:", objectWithArray);
+// Output: { name: 'Sample Object', items: ['Apple', 'Banana', 'Orange'] }
