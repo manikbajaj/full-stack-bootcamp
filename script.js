7020c33e3178a288b18b67fb40b266c26e350e82
Manik <manik@pixelconception.com>
2024-09-26 13:01:02 +0530

    Updating code

--- script.js
+++
@@ -1,6 +0,0 @@
-const calculatorDisplay = document.querySelector("h1");
-const inputBtns = document.querySelectorAll("button");
-const clearBtn = document.getElementById("clear-btn");
-
-let firstValue = 0;
-let awaitingNextValue = false;
