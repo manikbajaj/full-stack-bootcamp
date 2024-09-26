6dc74f28a2fffe31225239911ef886b793a2d2bb
Manik <manik@pixelconception.com>
2024-09-26 18:00:20 +0530

    Final code for FSDB477

---
+++ src/values.js
@@ -0,0 +1,27 @@
+let firstValue = 0;
+let operatorValue = "";
+let awaitingNextValue = false;
+
+export function getFirstValue() {
+  return firstValue;
+}
+
+export function setFirstValue(value) {
+  firstValue = value;
+}
+
+export function getOperatorValue() {
+  return operatorValue;
+}
+
+export function setOperatorValue(value) {
+  operatorValue = value;
+}
+
+export function getAwaitingNextValue() {
+  return awaitingNextValue;
+}
+
+export function setAwaitingNextValue(value) {
+  operatorValue = value;
+}
