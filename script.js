d6f4990229c3ffd29d08329a9be605f89cd833d0
Manik <manik@pixelconception.com>
2024-09-26 13:07:01 +0530

    Final code for FSDB462

--- script.js
+++ script.js
@@ -4,3 +4,24 @@ const clearBtn = document.getElementById("clear-btn");
 
 let firstValue = 0;
 let awaitingNextValue = false;
+
+function addNumberValue(number) {
+  // Replace current display value if first value is entered
+  if (awaitingNextValue) {
+    calculatorDisplay.textContent = number;
+    awaitingNextValue = false;
+  } else {
+    // If current display value is 0, replace it, if not add number to display value
+    const displayValue = calculatorDisplay.textContent;
+    calculatorDisplay.textContent =
+      displayValue === "0" ? number : displayValue + number;
+  }
+}
+
+// Add Event Listeners for numbers, operators, decimal
+// Add event listeners one at a time
+inputBtns.forEach((inputBtn) => {
+  if (inputBtn.classList.length === 0) {
+    inputBtn.addEventListener("click", () => addNumberValue(inputBtn.value));
+  }
+});
