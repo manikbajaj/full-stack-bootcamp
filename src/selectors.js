1b8bfae71493133f98090c32d38804a8bcb31603
Manik <manik@pixelconception.com>
2024-09-27 12:17:01 +0530

    creating a starting point

--- src/selectors.js
+++
@@ -1,3 +0,0 @@
-export const calculatorDisplay = document.querySelector("h1");
-export const inputBtns = document.querySelectorAll("button");
-export const clearBtn = document.getElementById("clear-btn");
