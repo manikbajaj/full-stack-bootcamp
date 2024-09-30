6ddc9b32f12b03d9dce7b63774fb453ab1f9e98e
Manik <manik@pixelconception.com>
2024-09-30 20:48:29 +0530

    Final code for FSDB517

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
-function loadFile() {
-  // Fetch always returns a Promise
-  fetch("https://jsonplaceholder.typicode.com/posts/1")
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      console.log(data); // Assuming the task is to log the data
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+/** 
+ * 
+    async function myFunction() {
+      return "Hello";
+    }
+
+    IS THE SAME AS 
+
+  function myFunction() {
+    return Promise.resolve("Hello");
+  }
+
+    myFunction().then(
+      function(value),  // code if successful await keyword only caters to success
+    );
+
+    IS THE SAME AS 
+    
+    let value = await myFunction();
+ * 
+ */
+
+async function loadFile() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+  const data = await response.json();
+  console.log(data); // Log the data
 }
 
 loadFile();
